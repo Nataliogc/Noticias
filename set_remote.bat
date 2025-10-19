ee0d5b55a89a63710e24989ea13b35553e0ee5d6
nataliohoteles-crypto <comunicaciones@hotelguadiana.es>
2025-10-19 16:45:01 +0200

    auto: publicacion Noticias (2025-10-19_164500)

--- set_remote.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "REMOTE_URL=https://github.com/Nataliogc/Noticias.git"
-git remote remove origin >nul 2>&1
-git remote add origin "%REMOTE_URL%"
-echo Remoto origin configurado: %REMOTE_URL%
-pause
